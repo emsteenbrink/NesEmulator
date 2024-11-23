d7be30c7c3b3c6101f36d2a520c6b2908de2bd7f
Ernst Steenbrink <ernst@steenbrink.nl>
2024-11-23 18:14:10 +0100

    Changed from vs solution/projects to CMAKE.

---
+++ NesEmulator/source/Main.cpp
@@ -0,0 +1,15 @@
+#include "NesEmulator.h"
+
+#include <SDL.h>
+#include <chrono>
+#include <memory>
+
+extern uint8_t g_overrrun;
+extern uint8_t g_underrun;
+
+int main(int argc, char* argv[])
+{
+  auto nesEmulator = std::make_unique<NesEmulator>();
+  nesEmulator->Run();
+  return 0;
+}
